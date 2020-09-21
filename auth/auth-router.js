c49eeb9dd6d3a497d8bd6026dfb8a57aced1b91e
Joe Bay <jbay55278@gmail.com>
2020-09-21 11:29:58 -0800

    built the auth-middleware and added user_id to the respone from logging in

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -31,7 +31,7 @@ router.post('/login', (req, res) => {
           if(user && bcrypt.compareSync(password, user.password)){
             const token = signToken(user)
   
-            res.status(200).json({ message: "welcome to your recipe app!", token })
+            res.status(200).json({ message: "welcome to your recipe app!", token, user_id: user.id })
           } else {
             res.status(401).json({ message: "Invalid credentials" })
           }
